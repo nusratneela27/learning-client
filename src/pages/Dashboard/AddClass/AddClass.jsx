0fb4f5fb483d5058fde74fa79b4d9f653132405c
neela <nusratneela27@gmail.com>
2023-06-13 21:51:54 +0600

    payment updated

--- src/pages/Dashboard/AddClass/AddClass.jsx
+++ src/pages/Dashboard/AddClass/AddClass.jsx
@@ -1,9 +1,98 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import useAuth from '../../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import { Helmet } from 'react-helmet-async';
 
 const AddClass = () => {
+    const { user, } = useAuth();
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+
+        fetch("http://localhost:5000/class", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.insertedId) {
+                    console.log(data);
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Class Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                    reset();
+                }
+            });
+
+    }
+
     return (
-        <div>
-            <h2>add class</h2>
+        <div className="w-full px-10">
+            <Helmet>
+                <title>e Crft | Add Class</title>
+            </Helmet>
+            <SectionTitle
+                heading={"Add a class"}
+                subheading={"New Class"}
+            ></SectionTitle>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control w-full mb-4">
+                    <label className="label">
+                        <span className="label-text font-semibold">Class Name</span>
+                    </label>
+                    <input type="text" placeholder="class Name"
+                        {...register("course", { required: true, maxLength: 120 })}
+                        className="input input-bordered w-full " />
+                </div>
+
+                <div className="flex my-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-semibold">Instructor</span>
+                        </label>
+                        <input type="text" placeholder="class Name"
+                            {...register("instructor", { required: true })}
+                            className="input input-bordered w-full " defaultValue={user?.displayName} />
+                    </div>
+                    <div className="form-control w-full ml-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Instructor Email</span>
+                        </label>
+                        <input type="email" placeholder="Instructor email"
+                            {...register("instructorEmail", { required: true })}
+                            className="input input-bordered w-full " value={user?.email} />
+                    </div>
+                </div>
+                <div className="flex my-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-semibold">Available seat</span>
+                        </label>
+                        <input type="number" {...register("seats", { required: true })} placeholder="Available seat" className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full ml-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Price</span>
+                        </label>
+                        <input type="number" {...register("price", { required: true })} placeholder="Type here" className="input input-bordered w-full " />
+                    </div>
+                </div>
+                <div className="form-control w-full mb-4">
+                    <label className="label">
+                        <span className="label-text font-semibold">Image link</span>
+                    </label>
+                    <input type="url" placeholder="image link"
+                        {...register("image", { required: true })}
+                        className="input input-bordered w-full " />
+                </div>
+                <input className="btn btn-sm mt-4" type="submit" value="Add Item" />
+            </form>
         </div>
     );
 };
--- src/pages/Dashboard/MyClass/MyClass.jsx
+++ src/pages/Dashboard/MyClass/MyClass.jsx
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
+import { FaPencilAlt } from 'react-icons/fa';
+import { Helmet } from 'react-helmet-async';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const MyClass = () => {
+    const { user } = useAuth();
+    const [myClass, setMyClass] = useState([]);
+
+    const url = `http://localhost:5000/class?email=${user?.email}`
+    console.log(user);
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMyClass(data))
+    }, [url])
+
     return (
-        <div>
-            <h2>my class</h2>
+        <div className='w-full max-w-screen-xl mx-auto'>
+            <Helmet>
+                <title>e Crft | My Class</title>
+            </Helmet>
+            <SectionTitle
+                heading={"My Classes"}
+                subheading={"All My Classes"}
+            ></SectionTitle>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full mt-5 px-8">
+                    {/* head */}
+                    <thead className='bg-sky-800 text-white px-8'>
+                        <tr>
+                            <th>#</th>
+                            <th>Images</th>
+                            <th>Class Name</th>
+                            <th>Email</th>
+                            <th>Price</th>
+                            <th className='text-end'>Updated</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myClass.map((item, index) => <tr
+                                key={item._id}
+                            >
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.instructor}
+                                </td>
+                                <td>
+                                    {item.instructorEmail}
+                                </td>
+                                <td className="">${item.price}</td>
+                                <td className='text-end'>
+                                    <button onClick={() => handleDelete(item)} className="btn btn-ghost bg-sky-800 text-white"><FaPencilAlt></FaPencilAlt></button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
